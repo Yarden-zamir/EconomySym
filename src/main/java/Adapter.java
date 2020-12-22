9252f006976041d961a98c762b1f2b4492395a90
Yarden zamir <PandaBoy444@gmail.com>
2020-12-22 09:19:01 +0200

    Added interpreter to read sources

--- src/main/java/Adapter.java
+++ src/main/java/Adapter.java
@@ -2,23 +2,71 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Adapter {
-	public static void main(String args){
-		Company Milqe = new Company("Milqe");
+	public static void main(String[] args) {
+		Company Milqe;
+		String sources = "<minecraft:iron_ore> * 4;" +
+				"<minecraft:raw_beef>;" +
+				"<minecraft:leather>;" +
+				"<minecraft:milk>;" +
+				"<minecraft:rose> * 3;";
+		interpreter.retrieveItems(sources);
 
 	}
 
 }
-class Company{
-	ArrayList<item> needs = new ArrayList<item>(); // things they need for production
-	HashMap<item, ArrayList<item>> makes; //things they make
-	Company(String name){
 
+class Company {
+	ArrayList<item> sources = new ArrayList<item>(); // things they source
+	ArrayList<recipe> recipes = new ArrayList<>(); // what they can make
+	String name;
+
+	public Company(ArrayList<item> sources, ArrayList<recipe> recipes, String name) {
+		this.sources = sources;
+		this.recipes = recipes;
+		this.name = name;
+	}
+
+	public Company(String name) {
+		this.name = name;
 	}
 }
 
-class item{
-	int weight=1;
-	item(String name, int weight){
+class interpreter {
+	public static ArrayList<item> retrieveItems(String input) {
+		ArrayList<item> output = new ArrayList<>();
+		for (String s : input.split(";")) {
+			if (s.contains("*")) {
+				output.add(new item(s.split("\\*")[0], Integer.parseInt(s.split("\\*")[1].trim())));
+			} else {
+				output.add(new item(s, 1));
+			}
+		}
+		return output;
+	}
+
+}
+
+class recipe {
+	ArrayList<item> ingredients = new ArrayList<>();
+	item output;
+
+	public recipe(ArrayList<item> ingredients, item output) {
+		this.ingredients = ingredients;
+		this.output = output;
+	}
+}
+
+class item {
+	int weight = 1;
+	String name;
+
+	@Override
+	public String toString() {
+		return name+" * "+weight;
+	}
+
+	item(String name, int weight) {
+		this.name = name;
 		this.weight = weight;
 	}
 }
